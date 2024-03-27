10c849f99d9b4add23386faad61a7bb83801c59e
Aspergius <mr.tonylev@icloud.com>
2024-03-27 19:08:11 +0200

    exr2.8 add phone numbers

--- fullstuck_part2/phonebook/src/App.jsx
+++ fullstuck_part2/phonebook/src/App.jsx
@@ -1,32 +1,40 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "040-1234567" }]);
   const [newName, setNewName] = useState("");
+  const [number, setNumber] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
-    console.log("check new name",newName)
+    console.log("check new name/number",newName, number);
     
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
       setNewName("");
+      setNumber("");
     } else {
       const newPerson = {
         name: newName,
+        number: number,
       };
       setPersons((previous) => [...previous, newPerson]);
       setNewName("");
+      setNumber("");
     }
     
   };
 
   const handleChange = (event) => {
-    console.log(event.target.value);
+    console.log("for a name:",event.target.value);
     setNewName(event.target.value);
-
     
   };
+
+  const handleNumberChange = (event) => {
+    console.log("for number:",event.target.value)
+    setNumber(event.target.value);
+  }
 
   return (
     <div>
@@ -37,6 +45,9 @@ const App = () => {
           name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
+          number: <input value={number} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
@@ -44,7 +55,7 @@ const App = () => {
         <div>
           {persons.map(( person,index ) => (
             <div key={index}>
-              <p>{person.name}</p>
+              <p>{person.name} {person.number}</p>
             </div>
           ))}
         </div>
