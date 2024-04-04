64fc4ff6d5704a5832e1d7b6d30f60cfc5cad2b0
Aspergius <mr.tonylev@icloud.com>
2024-04-04 20:22:31 +0300

    exr 2.10 refactoring components into components folder

--- fullstuck_part2/phonebook/src/App.jsx
+++ fullstuck_part2/phonebook/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+import Filter from "./components/Filter";
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -46,7 +49,6 @@ const App = () => {
     let searchedValue = searchedPhoneNumber.value;
     console.log("thats what im searching", searchedValue);
 
-
     //this has a subtle bug,function searches any char in any number
     //thus showing every num that has 'a' e.g.
     setFilter(
@@ -64,41 +66,11 @@ const App = () => {
     <div>
       <div>debug: {newName}</div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with{" "}
-        <input type="text" id="searchRequest" onInput={numbersToShow} />
-      </div>
-      <div>
-        {showFilter.map((filtered) => (
-          <div key={filtered.id}>
-            <p>
-              {filtered.name} {filtered.number}{" "}
-            </p>
-          </div>
-        ))}
-      </div>
+      <Filter numbersToShow={numbersToShow} showFilter={showFilter} />
       <h2>Add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleChange} />
-        </div>
-        <div>
-          number: <input value={number} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm addPerson={addPerson} newName={newName} number={number} handleChange={handleChange} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      <div>
-        {persons.map((person, index) => (
-          <div key={index}>
-            <p>
-              {person.name} {person.number}
-            </p>
-          </div>
-        ))}
-      </div>
+      <Persons persons={persons} />
     </div>
   );
 };
