c86c419c83d301e74795f1d61c940ec511a931db
Aspergius <mr.tonylev@icloud.com>
2024-04-14 20:28:32 +0300

    exr.2.11 getting data from server

--- fullstuck_part2/frontend_part2/src/App.jsx
+++ fullstuck_part2/frontend_part2/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Note from "./components/note";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    console.log("effect")
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log("promise fulfilled")
+        setNotes(response.data)
+      })
+  }, [])
+  console.log("render", notes.length, "notes")
 
   const notesToShow = showAll
     ? notes
@@ -14,7 +26,7 @@ const App = (props) => {
     event.preventDefault();
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5,
+      important: Math.random() > 0.5,
       id: notes.length + 1,
     };
     setNotes(notes.concat(noteObject));
